08492154cf10d3639674181a83c995ff76973683
kolishchak <bohdan.kolishchak@gmail.com>
2017-12-29 04:58:59 +0200

    Added alias attribute to the items table.

---
+++ db/migrate/20171229025546_add_alias_to_items.rb
@@ -0,0 +1,5 @@
+class AddAliasToItems < ActiveRecord::Migration[5.1]
+  def change
+    add_column :items, :alias, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229024123) do
+ActiveRecord::Schema.define(version: 20171229025546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171229024123) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.string "alias"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
