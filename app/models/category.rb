c346c60f5b4494334111eb896f40dfb0be2c393a
kolishchak <bohdan.kolishchak@gmail.com>
2017-12-30 23:51:04 +0200

    FriendlyId for Category, validates enabled.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,6 @@ class Category < ApplicationRecord
   has_many :items
 
   extend FriendlyId
-  friendly_id :name, use: [:slugged, :finders], slug_column: :alias
+  friendly_id :name, use: [:slugged, :finders]
+  validates :slug, presence: true, uniqueness: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
+
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders]
+  validates :slug, presence: true, uniqueness: true
 end
