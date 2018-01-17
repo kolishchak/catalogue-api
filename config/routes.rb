522525007a51750837f81c82124e7b11bb867ae5
kolishchak <bohdan.kolishchak@gmail.com>
2018-01-17 16:13:07 +0200

    Adding collection routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :items
 
-  get 'items_count' => 'items#count'
+  resources :categories
+
+  resources :items do
+    get 'count', :on => :collection
+  end
 
 end
