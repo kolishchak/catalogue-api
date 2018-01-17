d09cc4617aa4980d8345a0c89ed6df0f4619b64d
kolishchak <bohdan.kolishchak@gmail.com>
2018-01-17 16:09:56 +0200

    Refactoring controller methods.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,19 @@
 class ItemsController < ApplicationController
-  def index
-    @items = Item.by_category(params[:category_slug])
-                 .paginate(:page => params[:page], :per_page => 5)
-    render json: @items
-  end
 
-  def count
-    @count = Item.calculate(params[:category_slug])
-    render json: @count
+  def index
+    items = params[:category_slug] ? Item.by_category(params[:category_slug]) : Item.all
+    items = items.paginate(page: params[:page], :per_page => 5)
+    render json: items
   end
 
   def show
-    @item = Item.friendly.find(params[:id])
-    render json: @item
+    item = Item.friendly.find(params[:id])
+    render json: item
+  end
+
+  def count
+    records = Item.calculate(params[:category_slug])
+    render json: records
   end
 
 end
-
